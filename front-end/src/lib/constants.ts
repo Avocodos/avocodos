3b7cf2852e154ddeb31dd2909c6e5ddb067ee36d
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-05 00:47:44 +0530

    add: constants

--- front-end/src/lib/constants.ts
+++ front-end/src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const SYSTEM_USER_ID = "oha3nykpq3sa3muk";
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const SITE_NAME = "Avocodos";
+export const SITE_DESCRIPTION = "Avocodos is a completely web3-based social media and learning platform, built on top of the Aptos blockchain and aimed towards all upcoming and aspiring developers.";
+export const SITE_URL = "https://avocodos.com";
