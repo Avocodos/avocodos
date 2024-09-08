7a3593ebe01ab9a9c4e9147b3a1df80c8e5cacdd
HarjjotSinghh <harjjotsinghh@gmail.com>
2024-09-09 02:53:45 +0530

    fix: build error

--- front-end/src/app/(main)/messages/layout.tsx
+++ front-end/src/app/(main)/messages/layout.tsx
@@ -1,15 +1,7 @@
-"use client";
-import { BASE_URL } from "@/lib/constants";
-import { useWeavy } from "@weavy/uikit-react";
-
 export default function MessagesLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const weavy = useWeavy({
-    url: process.env.NEXT_PUBLIC_WEAVY_URL,
-    tokenUrl: `${BASE_URL}/api/weavy/token`
-  });
   return <>{children}</>;
 }
